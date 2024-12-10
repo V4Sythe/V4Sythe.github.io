dcc05482f237421d446140f11b7232fe9de1632d
V4Sythe <vanceff5@gmail.com>
2024-12-10 18:06:13 +0000

    Finalized platformer game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,7 +28,7 @@ $(function () {
      * Comment the function call out to remove the grid
      */
 
-    // drawGrid();
+    drawGrid();
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -39,27 +39,32 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
-
-
+createPlatform(5, 200, 200, 25)
+createPlatform(350, 200, 200, 25)
+createPlatform(650, 350, 150, 25)
+createPlatform(850, 220, 100, 25)
+createPlatform(1100, 190, 100, 25)
+    
+    
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
+createCollectable ("steve", 450, 150)
+createCollectable ("database", 1150, 150)
+createCollectable ("max", 900, 170)
     
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
-    
+createCannon ("top", 100, 500)
+createCannon ("bottom", 450, 1000)
+createCannon ("right", 200, 2200)
+createCannon ("bottom", 650, 1600)
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
